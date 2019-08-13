3598a549b2cbc8e57151d48569a4aed72b54de9e
Nazar <nvoloshchuk19@gmail.com>
2019-08-13 19:46:46 +0300

    fix sign in page

--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -1,11 +1,10 @@
 from locators import LocatorsSignIn
 from credentials import Credentials
 
-import inspect
-
 
 class SignInPage:
     """Sign in page with login method for any type of person"""
+
     def __init__(self, browser):
         self.browser = browser
         self.locators = LocatorsSignIn
@@ -17,10 +16,6 @@ class SignInPage:
         self.browser.clear_element(self.locators.PASSWORD)
         self.browser.send_keys(self.locators.PASSWORD, passwd)
 
-    # def click_sign_in(self):
-    #     """Clicks 'Sign In' button"""
-    #     self.browser.click_element(self.locators.SIGN_IN)
-
     def login(self, person):
         """Logins any person. It starts from homepage for unlogined person"""
         self.enter_credentials(*Credentials[person])
@@ -28,21 +23,12 @@ class SignInPage:
 
     def enter_sign_in_email(self, email):
         self.browser.send_keys(self.locators.EMAIL_SIGN_IN, email)
-        for i in inspect.stack():
-            print('====debug===', i[2], i[3], i[4])
 
     def enter_sign_in_password(self, password):
         self.browser.send_keys(self.locators.PASSWORD_SIGN_IN, password)
 
     def click_sign_in(self):
         self.browser.click_element(self.locators.BUTTON_SIGN_IN)
-
-    # def login_user(self, username, password):
-    #     self.header.click_icon()
-    #     self.header.click_log_in()
-    #     self.enter_sign_in_email(username)
-    #     self.enter_sign_in_password(password)
-    #     self.click_sign_in()
 
     def click_sign_up_tab(self):
         self.browser.click_element_by_text(self.locators.TAB_SIGN_UP, "Sign Up")
@@ -80,4 +66,3 @@ class SignInPage:
     def is_psswords_mismatch(self):
         text = self.browser.get_text_of_element(self.locators.PASSWORD_MATCHING_SIGN_UP_WRONG)
         return text == "Passwords don`t match!"
-
