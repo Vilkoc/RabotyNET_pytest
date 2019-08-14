bf9c1400c25ed8a75395e3d2cc8d414409c24d32
natalyshevchuk <nshvchk24@gmail.com>
2019-08-14 17:31:32 +0300

    with allure

--- tests/test_delete_company.py
+++ tests/test_delete_company.py
@@ -8,7 +8,8 @@ def test_delete_company(app):
     with allure.step("Login"):
         app.header.select_option(CownerData.OPTION)
         app.sign_in_page.login(CownerData.PERSON)
-    app.header.click_my_companies()
+    with allure.step("All companies"):
+        app.header.click_my_companies()
     app.my_companies_page.click_company_delete(CownerData.COMPANY_DELETE)
     check = app.my_companies_page.check_company_present(CownerData.COMPANY_DELETE)
     assert check
