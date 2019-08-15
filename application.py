9d88f9c426e3bfb2600460aa5d05b09cd58ac9e7
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-08-15 18:54:59 +0300

    Second Dima commit

--- application.py
+++ application.py
@@ -1,12 +1,6 @@
 from pages.header import Header
 from pages.sign_in_page import SignInPage
 from pages.user_profile_page import UserPage
-from pages.vacancies_page import VacanciesPage
-from pages.view_vacancy_page import ViewVacancyPage
-from pages.preview_resume_page import PreviewResumePage
-from pages.edit_resume_page import EditResumePage
-from pages.view_company_page import ViewCompanyPage
-from pages.companies_page import CompaniesPage
 
 
 class SeleniumTestBase():
@@ -17,9 +11,4 @@ class SeleniumTestBase():
         self.header = Header(self.driver_init)
         self.sign_in_page = SignInPage(self.driver_init)
         self.user_profile_page = UserPage(self.driver_init)
-        self.vacancies_page = VacanciesPage(self.driver_init)
-        self.view_vacancy_page = ViewVacancyPage(self.driver_init)
-        self.preview_resume_page = PreviewResumePage(self.driver_init)
-        self.edit_resume_page = EditResumePage(self.driver_init)
-        self.view_company_page = ViewCompanyPage(self.driver_init)
-        self.companies_page = CompaniesPage(self.driver_init)
+
