36c615d9cc3664bc652cec407b37516ff29472f5
Vilkoc <vilkoc@gmail.com>
2019-08-20 17:29:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -31,4 +31,4 @@ USERNAME_PASSW_RECOVERY = 'rabotynet.test.fp@gmail.com'
 OLD_PASSWORD = 'Qdrwbj!23'
 NEW_PASSWORD = 'Qdrwbj1@3'
 
-TOMCAT_PATH = ''
+TOMCAT_PATH = 'C:/Users/sdiedtc/Documents/web/apache-tomcat-9.0.10/bin/'
