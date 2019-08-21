0e48bbfefc5d11623b618ca6cb40f718b0e9bc17
Vilkoc <vilkoc@gmail.com>
2019-08-21 17:58:31 +0300

    cosmetic fixes

--- tests/test_login_Stas.py
+++ tests/test_login_Stas.py
@@ -1,31 +1,31 @@
+""" This module automate testing for sign in users (user, cowner, admin) """
 import pytest
-
 import allure
+
 
 @allure.feature('Sign In')
 @pytest.mark.parametrize('user, password, expected', [
     ('admin@gmail.com', 'admin', 'Сompanies'),
-    ('user@gmail.com', 'user', 'Create company1'),
+    ('user@gmail.com', 'user', 'Create company'),
     ('cowner@gmail.com', 'cowner', 'My companies')
-    ])
+])
 def test_login_logout(app, make_screen, user, password, expected):
     """ Testcase for user login.
         Try to login for each user"""
-    page = app
 
-    page.header.click_icon()
-    page.header.click_log_in()
+    app.header.click_icon()
+    app.header.click_log_in()
 
-    page.sign_in_page.enter_sign_in_email(user)
-    page.sign_in_page.enter_sign_in_password(password)
-    page.sign_in_page.click_sign_in()
+    app.sign_in_page.enter_sign_in_email(user)
+    app.sign_in_page.enter_sign_in_password(password)
+    app.sign_in_page.click_sign_in()
 
     with allure.step('Check if user logined'):
-        assert page.header.is_logined(), "No 'log out' link"
-        page.header.click_icon()
+        assert app.header.is_logined(), "No 'log out' link"
+        app.header.click_icon()
 
     with allure.step('Check if user authorized'):
-        assert page.header.get_text_of_first_link() == expected, 'Wrong link text'
+        assert app.header.get_text_of_first_link() == expected, 'Wrong link text'
 
-    page.header.click_icon()
-    page.header.click_log_out()
+    app.header.click_icon()
+    app.header.click_log_out()
