4cc4d2e057073078f733f142225030b204c2693d
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-08-22 19:42:12 +0300

    Dima made docstrins

--- tests/test_login_all.py
+++ tests/test_login_all.py
@@ -1,3 +1,4 @@
+"""Checks login for all persons: admin, cowner, user"""
 import allure
 from credentials import Credentials
 import pytest
@@ -6,6 +7,7 @@ import pytest
 @allure.feature('Login all users')
 @pytest.mark.parametrize('person', Credentials.keys())
 def test_login_all(app, person):
+    """Reads person credentials and tries to login for all types of persons"""
     with allure.step('Check log in for all'):
         app.header.select_option('Log in')
         app.sign_in_page.login(person)
