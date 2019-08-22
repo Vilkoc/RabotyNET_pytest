99cfed044871371c43beac42ea6b7b77790b5da6
natalyshevchuk <nshvchk24@gmail.com>
2019-08-22 18:57:17 +0300

    Nya

--- tests/test_user_profile_positive.py
+++ tests/test_user_profile_positive.py
@@ -1,11 +1,14 @@
-import allure
+"""Perform positive tests for all fields of user profile page"""
 import pytest
+import allure
 from data_tests.user_data import user_data_rab_19 as entry
 
 
 @allure.feature('Login all users')
 @pytest.mark.parametrize('field', entry)
 def test_all(app, get_to_user_profile, field):
+    """Enters into particular field the valid data, clicks 'Update button', reads submitted data from the\
+     field and checks for validity"""
     with allure.step('Click log in'):
         valid_entry = app.user_profile_page.enter_data_textbox(field, entry[field])
         app.user_profile_page.click_update_profile()
