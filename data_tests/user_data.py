fec959c755ef00cfd7b95a85d391def3bdc5cb68
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-09-03 22:33:08 +0300

    10 users

--- data_tests/user_data.py
+++ data_tests/user_data.py
@@ -1,16 +1,16 @@
 """This module contains company owner data"""
 
 user_data_rab_19 = {
-    'FIRST_NAME': 'Andrew',
-    'LAST_NAME': 'Stuart',
-    'EMAIL': 'a@chnu.edu.ua',
-    'PHONE': '+380971465477',
-    'COUNTRY': 'Ukraine',
-    'CITY': 'Chernivtsi',
-    'STREET': 'Holovna',
-    'BUILDING': '555',
-    'APARTMENT': '14',
-    'ZIP_CODE': '78741'
+    'FIRST_NAME': (1, 'Andrew'),
+    'LAST_NAME': (2, 'Stuart'),
+    'EMAIL': (3, 'a@chnu.edu.ua'),
+    'PHONE': (4, '+380971465477'),
+    'COUNTRY': (5, 'USA'),
+    'CITY': (6, 'Dallas'),
+    'STREET': (7, 'Hobby Horse'),
+    'BUILDING': (8, '555'),
+    'APARTMENT': (9, '14'),
+    'ZIP_CODE': (10, '78741')
 }
 
 user_data_rab_26 = {
@@ -24,3 +24,4 @@ user_data_rab_26 = {
     'APARTMENT': '14-Q',
     'ZIP_CODE': '787414'
 }
+
--- tests/test_user_profile_positive.py
+++ tests/test_user_profile_positive.py
@@ -6,13 +6,17 @@ from data_tests.user_data import user_data_rab_19 as entry
 
 @allure.feature('Positive test')
 @pytest.mark.parametrize('field', entry)
-def test_all(app, get_to_user_profile, field):
+def test_all(app, field):
     """Enters into particular field the valid data, clicks 'Update button', reads submitted data
      from the field and checks for validity"""
     with allure.step('Positive test for user profile data'):
-        valid_entry = app.user_profile_page.enter_data_textbox(field, entry[field])
+        user_num = entry[field][0]
+        app.header.select_option('Log in')
+        app.sign_in_page.login('USER_' + str(user_num))
+        app.header.select_option('Profile')
+        valid_entry = app.user_profile_page.enter_data_textbox(field, entry[field][1])
         app.user_profile_page.click_update_profile()
         read = app.user_profile_page.read_data_textbox(field)
         assert valid_entry
-        assert read == entry[field]
-        app.header.select_option('Log out')
+        assert read == entry[field][1]
+
